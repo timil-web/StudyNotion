c858e6fc61ec61ec24622f962c6d27fac15a0f17
timil <timilvaishnav@gmail.com>
2025-11-15 11:06:26 +0530

    did otp changes

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User")
-const OTP = require("../models/Otp")
+const OTP = require("../models/OTP")
 const Profile = require("../models/Profile")
 const otpGenerator =require("otp-generator")
 const bcrypt = require("bcrypt")
