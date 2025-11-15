33336993c71ea36ec6ac3f0894941f10b64f15bc
timil <timilvaishnav@gmail.com>
2025-11-15 11:55:45 +0530

    add frontend url at index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: process.env.FRONTEND_URL,
 		credentials: true,
 	})
 );
