105a595069afd8e84ccd6534d3ba6639f97ffcba
timil <timilvaishnav@gmail.com>
2025-12-15 16:32:05 +0530

    middleware changes in index.js

--- server/index.js
+++ server/index.js
@@ -21,18 +21,22 @@ const PORT = process.env.PORT || 4000;
 // database connection
 database.connect();
 // MIDDLEWARE
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
 	cors({
 		origin: process.env.FRONTEND_URL,
 		credentials: true,
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
-        exposedHeaders: ['Set-Cookie']
+        exposedHeaders: ['Set-Cookie'],
+		preflightContinue: false,
+		optionsSuccessStatus: 204
 	})
 );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use(
 	fileUpload({
 		useTempFiles: true,
