c5ef23d8ccb3d93da9c92ec1791c8606fe429306
timil <timilvaishnav@gmail.com>
2026-05-01 19:32:57 +0530

    cookie for 3 to 7 days

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -201,7 +201,7 @@ exports.login = async(req,res) =>{
         user.password = undefined;
         //create cookie and send res
         const options = {
-            expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
             httpOnly: true,
             secure: true,
             sameSite: "None",
