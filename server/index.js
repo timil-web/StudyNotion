4e3a2b1ef5baa14946589d2e07fd39dd1bb94785
timil <timilvaishnav@gmail.com>
2026-07-03 11:45:03 +0530

    cors issue

--- server/index.js
+++ server/index.js
@@ -25,12 +25,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL?.replace(/\/$/, ""),
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://studynotion-frontend-seven-lime.vercel.app",
+  ],
+  credentials: true,
+}))
 
 app.use(
 	fileUpload({
